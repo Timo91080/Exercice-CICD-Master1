0bdef8b826235a0fe236fae1e80e73f4b4bea902
Timo91080 <diketetimothee2@gmail.com>
2026-02-13 12:45:36 +0100

    feat: add response to root route

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,13 @@ const port = process.env.PORT || 3000;
 const appName = process.env.APP_NAME || "mon-api";
 const environment = process.env.NODE_ENV || "development";
 
+app.get("/", (req: Request, res: Response) => {
+  res.json({
+    message: `Bienvenue sur ${appName} !`,
+    environment: environment,
+    timestamp: new Date().toISOString(),
+  });
+});
 app.get("/health", (req: Request, res: Response) => {
   res.status(200).json({
     status: "OK",
